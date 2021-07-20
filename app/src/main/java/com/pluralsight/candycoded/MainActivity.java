b86612c234c8f17e298cfe1f7fa9809765ac331c
Shubham Shukla <shuklas664@gmail.com>
2021-07-20 21:55:19 +0530

    Done

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pluralsight.candycoded;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.ContentValues;
 import android.content.Intent;
@@ -9,6 +10,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -22,7 +24,6 @@ import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.TextHttpResponseHandler;
 
 import cz.msebera.android.httpclient.Header;
-
 
 public class MainActivity extends AppCompatActivity {
   private Candy[] candies;
@@ -81,6 +82,14 @@ public class MainActivity extends AppCompatActivity {
   }
   // ***
   // TODO - Task 1 - Show Store Information Activity
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    Intent infoIntent = new Intent(this, InfoActivity.class);
+    startActivity(infoIntent);
+    return super.onOptionsItemSelected(item);
+  }
+
   // ***
 
   private void addCandiesToDatabase(Candy[] candies) {
